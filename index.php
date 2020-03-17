462f8dcbe3fec852c1db1a38d9839660e36bd174
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-03-17 15:47:35 +0100

    [Main] Added smarty example.

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 // load every thigns installed 
 require './vendor/autoload.php';
 
-use Cocur\Slugify\Slugify;
-$slugify = new Slugify();
+$smarty = new Smarty;
+$smarty->debugging = true;
 
-echo $slugify->slugify('Hello world!');
+$smarty->assign('site_title', 'La fin du monde');
+$smarty->assign('events', ['virus', 'home-office', 'confinement', 'formation PHP', 'famine', 'décès']);
+
+$smarty->display('templates/index.tpl');
